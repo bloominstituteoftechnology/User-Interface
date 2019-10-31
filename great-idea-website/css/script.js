258d7753f6c3de026e030c7994a678c76de2bbc7
kika <anamonteiro430@gmail.com>
2019-10-31 01:35:51 +0000

    some stretches

---
+++ great-idea-website/css/script.js
@@ -0,0 +1,14 @@
+<script>
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
